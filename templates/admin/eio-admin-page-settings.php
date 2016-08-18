b61f95df6cfb1e5ca065e23909f6a097c8d27d28
Toby Herbert <tobyherbert@nialtoservices.co.uk>
2016-08-18 12:28:01 +0100

    Added License

--- templates/admin/eio-admin-page-settings.php
+++ templates/admin/eio-admin-page-settings.php
@@ -6,8 +6,26 @@
  * @package    ExtractorIO
  * @subpackage Templates/Admin
  * @version    1.0.0
+ *
+ *
+ * Extractor IO - Admin Template - Settings Page
+ * Copyright (C) 2015 Nialto Services
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
- 
+
 // Ensure we are not being accessed directly.
 if (!defined('ABSPATH')) {
 	exit;
@@ -55,4 +73,3 @@ $connection_status = '<div class="' . $connection_class . '" style="' . $connect
 		<input type="submit" name="submit" id="submit" class="button button-primary" value="Save Settings">
 	</form>
 </div>
-	
